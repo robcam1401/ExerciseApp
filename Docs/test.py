b2590ba2ee9ac8d593f7c5e34a7243c59d616b4b
ZachNalepa <zachnalepa@gmail.com>
2024-02-07 14:24:55 -0600

    merges suck

--- Docs/test.py
+++ Docs/test.py
@@ -12,7 +12,6 @@ sys.path.insert(0,_CloudAPIs)
 sys.path.insert(0,_libs)
 
 # import from path variables
-<<<<<<< HEAD
 import CloudAPIs.sqlInterface as l
 
 print(os.getcwd())
@@ -20,11 +19,9 @@ os.chdir("CSC405")
 print(os.getcwd())
 
 m = l.query.account_email(1)
-=======
 from CloudAPIs.sqlInterface import *
 
 m = query.account_email(1)
->>>>>>> 8387eb1308925c1ecfd7788264dae94fd9b9dabc
 
 for i in m:
     print(i)
